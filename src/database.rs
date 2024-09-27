3098b4ae93efe2c533d2330c4a589c470feea35d
user <feda.avdeev@yandex.ru>
2024-09-27 14:20:52 +0300

    add: db/add_order

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,7 @@
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
-use tokio::sync::futures;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres, Error};
+use sqlx::Row;
+
+use crate::model::{Item, WBmodel};
 
 #[derive(Clone)]
 pub(crate) struct DataBase {
@@ -27,9 +29,105 @@ impl DataBase {
 
     }
 
-    pub(crate) fn is_ok(&self) -> bool {
-        true
+    pub(crate) async fn add_order(&self, order: &WBmodel) -> Result<(), Error> {
+        let insert_delivery = r#"
+            INSERT INTO Delivery (name, phone, zip, city, address, region, email) 
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
+            RETURNING delivery_id;
+        "#;
+        let insert_payment = r#"
+        INSERT INTO payment (transaction, request_id, currency, provider, amount, payment_dt, bank, delivery_cost, goods_total, custom_fee)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+        "#;
+        let insert_orders = r#"
+        INSERT INTO orders (order_uid, track_number, entry, delivery_id, payment_id, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+        "#;
+
+        let mut tx = self.pool.begin().await?;
+
+        let delivery_id: i64 = sqlx::query(insert_delivery)
+            .bind(&order.delivery.name)
+            .bind(&order.delivery.phone)
+            .bind(&order.delivery.zip)
+            .bind(&order.delivery.city)
+            .bind(&order.delivery.address)
+            .bind(&order.delivery.region)
+            .bind(&order.delivery.email)
+            .fetch_one(&mut *tx)
+            .await?.get(0);
+
+        sqlx::query(insert_payment)
+            .bind(&order.payment.transaction)
+            .bind(&order.payment.request_id)
+            .bind(&order.payment.currency)
+            .bind(&order.payment.provider)
+            .bind(order.payment.amount)
+            .bind(order.payment.payment_dt)
+            .bind(&order.payment.bank)
+            .bind(order.payment.delivery_cost)
+            .bind(order.payment.goods_total)
+            .bind(order.payment.custom_fee)
+            .execute(&mut *tx)
+            .await?;
+
+        sqlx::query(insert_orders)
+            .bind(&order.order_uid)
+            .bind(&order.track_number)
+            .bind(&order.entry)
+            .bind(delivery_id)
+            .bind(&order.payment.transaction)
+            .bind(&order.locale)
+            .bind(&order.internal_signature)
+            .bind(&order.customer_id)
+            .bind(&order.delivery_service)
+            .bind(&order.shardkey)
+            .bind(order.sm_id)
+            .bind(&order.date_created)
+            .bind(&order.oof_shard)
+            .execute(&mut *tx)
+            .await?;
+
+        tx.commit().await?;
+        self.add_items(&order.order_uid, &order.items).await?;
+        Ok(())
     }
+
+    async fn add_items(&self, order_uid: &String, items: &Vec<Item>) -> Result<(), Error> {
+        let add_item = r#"
+            INSERT INTO item (chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
+        "#;
+        let add_item_mapping = r#"
+        INSERT INTO map_items (map_order_uid, map_chrt_id)
+            VALUES ($1, $2);
+        "#;
+        let mut tx = self.pool.begin().await?;
+
+        for i in items {
+            sqlx::query(add_item)
+            .bind(i.chrt_id)
+            .bind(&i.track_number)
+            .bind(i.price)
+            .bind(&i.rid)
+            .bind(&i.name)
+            .bind(i.sale)
+            .bind(&i.size)
+            .bind(i.total_price)
+            .bind(i.nm_id)
+            .bind(&i.brand)
+            .bind(i.status)
+            .execute(& mut *tx).await?;
+
+            sqlx::query(add_item_mapping)
+            .bind(order_uid)
+            .bind(i.chrt_id)
+            .execute(& mut *tx).await?;
+        }
+
+        tx.commit().await?;
+        Ok(())
+    } 
 
     pub(crate) async fn init(&self) {
         let create_delivery_db_request = r#"
@@ -82,15 +180,6 @@ impl DataBase {
         sqlx::query(create_item_db_request)
         .fetch_optional(&self.pool).await.expect("Can't init DB tables");
 
-        let create_map_items_db_request = r#"
-        CREATE TABLE IF NOT EXISTS map_items (
-            item_id BIGSERIAL PRIMARY KEY,
-            map_item_id BIGINT REFERENCES item ON DELETE SET NULL
-        );
-        "#;
-        sqlx::query(create_map_items_db_request)
-        .fetch_optional(&self.pool).await.expect("Can't init DB tables");
-
         let create_orders_db_request = r#"
         CREATE TABLE IF NOT EXISTS orders (
             order_uid VARCHAR(255) PRIMARY KEY,
@@ -98,7 +187,6 @@ impl DataBase {
             entry VARCHAR(255),
             delivery_id INT REFERENCES delivery ON DELETE SET NULL,
             payment_id VARCHAR(255) REFERENCES payment ON DELETE SET NULL,
-            items INT,
             locale VARCHAR(255),
             internal_signature VARCHAR(255),
             customer_id VARCHAR(255),
@@ -110,6 +198,16 @@ impl DataBase {
         );
         "#;
         sqlx::query(create_orders_db_request)
+        .fetch_optional(&self.pool).await.expect("Can't init DB tables");
+
+        let create_map_items_db_request = r#"
+        CREATE TABLE IF NOT EXISTS map_items (
+            map_order_uid VARCHAR(255) REFERENCES orders ON DELETE CASCADE,
+            map_chrt_id BIGINT REFERENCES item ON DELETE CASCADE,
+            PRIMARY KEY(map_order_uid, map_chrt_id)
+        );
+        "#;
+        sqlx::query(create_map_items_db_request)
         .fetch_optional(&self.pool).await.expect("Can't init DB tables");
     }
 
--- src/model.rs
+++ src/model.rs
@@ -1,57 +1,58 @@
 use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
 
 
-#[derive(Deserialize, Serialize)]
-struct Delivery {
-    name: String,
-    phone: String,
-    zip: String,
-    city: String,
-    address: String,
-    region: String,
-    email: String
+#[derive(Deserialize, Serialize, FromRow)]
+pub struct Delivery {
+    pub name: String,
+    pub phone: String,
+    pub zip: String,
+    pub city: String,
+    pub address: String,
+    pub region: String,
+    pub email: String
 }
-#[derive(Deserialize, Serialize)]
-struct Payment {
-    transaction: String,
-    request_id: String,
-    currency: String,
-    provider: String,
-    amount: u64,
-    payment_dt: u64,
-    bank: String,
-    delivery_cost: u64,
-    goods_total: u64,
-    custom_fee: u64
+#[derive(Deserialize, Serialize, FromRow)]
+pub struct Payment {
+    pub transaction: String,
+    pub request_id: String,
+    pub currency: String,
+    pub provider: String,
+    pub amount: i64,
+    pub payment_dt: i64,
+    pub bank: String,
+    pub delivery_cost: i64,
+    pub goods_total: i64,
+    pub custom_fee: i64
 }
-#[derive(Deserialize, Serialize)]
-struct Item {
-    chrt_id: u64,
-    track_number: String,
-    price: u64,
-    rid: String,
-    name: String,
-    sale: u64,
-    size: String,
-    total_price: u64,
-    nm_id: u64,
-    brand: String,
-    status: u64
+#[derive(Deserialize, Serialize, FromRow)]
+pub struct Item {
+    pub chrt_id: i64,
+    pub track_number: String,
+    pub price: i64,
+    pub rid: String,
+    pub name: String,
+    pub sale: i64,
+    pub size: String,
+    pub total_price: i64,
+    pub nm_id: i64,
+    pub brand: String,
+    pub status: i64
 }
 #[derive(Deserialize, Serialize)]
 pub struct WBmodel {
-    order_uid: String,
-    track_number: String,
-    entry: String,
-    delivery: Delivery,
-    payment: Payment,
-    items: Vec<Item>,
-    locale: String,
-    internal_signature: String,
-    customer_id: String,
-    delivery_service: String,
-    shardkey: String,
-    sm_id: u64,
-    date_created: String,
-    oof_shard: String
+    pub order_uid: String,
+    pub track_number: String,
+    pub entry: String,
+    pub delivery: Delivery,
+    pub payment: Payment,
+    pub items: Vec<Item>,
+    pub locale: String,
+    pub internal_signature: String,
+    pub customer_id: String,
+    pub delivery_service: String,
+    pub shardkey: String,
+    pub sm_id: i64,
+    pub date_created: String,
+    pub oof_shard: String
 }
